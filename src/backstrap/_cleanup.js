7fa8e6126a280af51b8fc419e698f59232708a11
Kevin Perry <perry@princeton.edu>
2014-02-28 06:15:35 -0500

    Re-factored JS.

--- src/backstrap/_cleanup.js
+++
@@ -1,7 +0,0 @@
-/*
- * Cleanup our temporary global context defn's. See _setup.js for explanation.
- */
-if (typeof this.define === "function" && this.define.amd) {
-	this._$$_backstrap_built_flag();
-	this.define("backstrap", ["jquery", "backbone", "bootstrap"], this.$$.noConflict);
-}
