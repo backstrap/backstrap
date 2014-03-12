98cdc864f6b6a6bf8becd49b773bc7f77e106d34
Kevin Perry <perry@princeton.edu>
2014-03-11 19:26:23 -0500

    examples

---
+++ examples.js
@@ -0,0 +1,40 @@
+require(['jquery', 'backbone', 'backstrap'], function($, Backbone, $$) {
+	
+	var bicycles = new Backbone.Collection([
+	    { type: 'mountain',   color: 'red',    purchased: '12/24/2013' },
+	    { type: 'road',       color: 'silver', purchased: '03/11/2013' },
+	    { type: 'cyclocross', color: 'blue',   purchased: '10/10/2011' },
+	    { type: 'hybrid',     color: 'white',  purchased: '07/30/2012' },
+	    { type: 'recumbent',  color: 'yellow', purchased: '08/01/2012' }
+	]);
+	
+	$('body').append(
+		$$.div({className: 'wrapper'},
+			$$.pageHeader($$.h1('Backstrap Examples'))
+		),
+		$$.jumbotron({bgcontext: 'primary'},
+			'Some example text in a Jumbotron, including ',
+			$$.span({context: 'warning'}, 'some text in a warning context color,'),
+			'a ',
+			$$.button('Button'),
+			' that doesn\'t do anything, and a star glyph: ',
+			$$.glyph('star')
+		),
+		$$.p('For the rest of the page, we will bind this dataset to various components:'),
+		$$.blockquote($$.code(
+			"var bicycles = new Backbone.Collection([\n",
+			"    { type: 'mountain',   color: 'red',    purchased: '12/24/2013' },\n",
+			"    { type: 'road',       color: 'silver', purchased: '03/11/2013' },\n",
+			"    { type: 'cyclocross', color: 'blue',   purchased: '10/10/2011' },\n",
+			"    { type: 'hybrid',     color: 'white',  purchased: '07/30/2012' },\n",
+			"    { type: 'recumbent',  color: 'yellow', purchased: '08/01/2012' }\n",
+			"]);"
+		)),
+		$$.p('Note that the widgets are all sharing the same data model objects, ',
+			'so changing a value with one widget will cause changes in the other widgets.'),
+		$$.div(
+			$$.label('Mountain bike color: '),
+			$$.TextField({model: bicycles[0], content: 'color'})
+		)
+	);
+});
