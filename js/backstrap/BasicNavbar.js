56d8c65908789d8d47a26609350b173023260bd9
Kevin Perry <perry@princeton.edu>
2014-03-27 12:02:30 -0500

    separate Navbar from List

--- js/backstrap/BasicNavbar.js
+++ js/backstrap/BasicNavbar.js
@@ -20,15 +20,74 @@
 				return this;
 			}
 		});
-		
-		var NavList = $$.List.extend({
+
+		var NavList = $$.CollectionView.extend({
 			className: 'navbar-collapse collapse',
-	
-			render: function () {
-				$$.List.prototype.render.apply(this, arguments);
-				this.$('> ul').addClass('nav navbar-nav');
-				return this;
-			}
+			
+			initialize : function(options) {
+				$$.CollectionView.prototype.initialize.call(this, options);
+				$(this.el).addClass('list');
+				_(this).bindAll('render');
+			},
+			
+		    render : function() {
+		    	$(this.el).empty();
+		    	this.itemViews = {};
+
+		    	this.collectionEl = $$.ul({className: 'nav navbar-nav'});
+
+		    	// if the collection is empty, we render the empty content
+		      if((!_(this.model).exists()  || this.model.length === 0) && this.options.emptyContent) {
+		        this._emptyContent = _(this.options.emptyContent).isFunction() ? 
+		          this.options.emptyContent() : this.options.emptyContent;
+		        this._emptyContent = $$.li(this._emptyContent);
+
+		        if(!!this._emptyContent) {
+		          this.collectionEl.appendChild(this._emptyContent);
+		        }
+		      }
+
+		      // otherwise, we render each row
+		      else {
+		        _(this.model.models).each(function(model, index) {
+		          var item = this._renderItem(model, index);
+		          this.collectionEl.appendChild(item);
+		        }, this);
+		      }
+
+		      this.el.appendChild(this.collectionEl);
+		      this._updateClassNames();
+
+		      return this;
+		    },
+
+		    // renders an item for the given model, at the given index
+		    _renderItem : function(model, index) {
+		      var content = null;
+		      if(_(this.options.itemView).exists()) {
+
+		        if(_(this.options.itemView).isString()) {
+		          content = this.resolveContent(model, this.options.itemView);
+		        }
+
+		        else {
+		          var view = new this.options.itemView(_({ model : model }).extend(
+		            this.options.itemViewOptions));
+		          view.render();
+		          this.itemViews[model.cid] = view;
+		          content = view.el;
+		        }
+		      }
+
+		      var item = $$.li(content);
+
+		      // bind the item click callback if given
+		      if(this.options.onItemClick) {
+		        $(item).click(_(this.options.onItemClick).bind(this, model));
+		      }
+
+		      return item;
+		    }
 		});
 	
 		return ($$.BasicNavbar = $$.BaseView.extend({
