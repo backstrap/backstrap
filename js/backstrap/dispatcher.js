b5a05f0aac7bc2bb30f31b885d562704df447856
Kevin Perry <perry@princeton.edu>
2014-09-24 21:22:34 -0400

    debugging.

--- js/backstrap/dispatcher.js
+++ js/backstrap/dispatcher.js
@@ -1,35 +1,36 @@
 (function(context) {
-	var fn = function($$, Backbone, $)
-	{
-		var activeInterval = 0;
+    var fn = function($$, $)
+    {
+        var timeout = null;
+        var activeInterval = 0;
         var lastRefresh = 99999999999;
-		var touchTime = (new Date()).getTime();
+        var touchTime = (new Date()).getTime();
 
-		var touch = function () {
-			console.log('touch');
-			touchTime = new Date().getTime();
-			if (touchTime - lastRefresh > dispatcher.minInterval) {
-				activeInterval = dispatcher.minInterval;
-				doRefresh();
-			}
-		};
+        var touch = function () {
+            console.log('touch');
+            touchTime = (new Date()).getTime();
+            if (touchTime - lastRefresh > dispatcher.minInterval) {
+                activeInterval = dispatcher.minInterval;
+                doRefresh();
+            }
+        };
 
-		var doRefresh = function doRefresh() {
-			var now = (new Date()).getTime();
-			
-			if (activeInterval < dispatcher.minInterval) {
-				activeInterval = dispatcher.minInterval;
-			}
-			if (now - touchTime > activeInterval*dispatcher.decayFrequency && activeInterval < dispatcher.maxInterval) {
-				activeInterval = Math.min(activeInterval*dispatcher.decayFactor, dispatcher.maxInterval);
-			}
-			if (timeout) {
-				clearTimeout();
-			}
-			timeout = setTimeout(doRefresh, activeInterval*1000);
-			lastRefresh = new Date().getTime();
-			this.trigger('refresh');
-		};
+        var doRefresh = function doRefresh() {
+            var now = (new Date()).getTime();
+            
+            if (activeInterval < dispatcher.minInterval) {
+                activeInterval = dispatcher.minInterval;
+            }
+            if (now - touchTime > activeInterval*dispatcher.decayFrequency && activeInterval < dispatcher.maxInterval) {
+                activeInterval = Math.min(activeInterval*dispatcher.decayFactor, dispatcher.maxInterval);
+            }
+            if (timeout) {
+                clearTimeout();
+            }
+            timeout = setTimeout(doRefresh, activeInterval*1000);
+            lastRefresh = (new Date()).getTime();
+            dispatcher.trigger('refresh');
+        };
 
         var dispatcher = $$.dispatcher = _.extend({
             minInterval: 10,
@@ -37,8 +38,12 @@
             decayFrequency: 4,
             decayFactor: 2,
 
-            /* Not normally used, but can be called OOB. */
             refresh: function () {
+                console.log('refresh!');
+            },
+
+            /* Not normally used, but can be called OOB. */
+            refreshMe: function () {
                 this.trigger('refresh');
             },
 
@@ -52,24 +57,23 @@
             stopRefresh: function (model) {
                 model.stopListening(this, 'refresh', model.refresh);
             }
-        }, Backbone.Events);
+        }, $$.Events);
 
-		$('html').on('click focus touchstart', function () {
-			touch();
-		});
-		
-		return dispatcher;
-	};
+        $('html').on('click focus touchstart', function () {
+            touch();
+        });
+        
+        return dispatcher;
+    };
 
-	if (typeof context.define === 'function' && context.define.amd &&
-			typeof context._$$_backstrap_built_flag === 'undefined') {
-		context.define('backstrap/dispatcher', ['backstrap', 'backbone', 'jquery'], fn);
-	} else if (typeof context.module === 'object' && typeof context.module.exports === 'object') {
-		context.module.exports = fn(require('backstrap'), require('backbone'), require('jquery'));
-	} else {
-		if (typeof context.$$ !== 'function') throw new Error('Backstrap environment not loaded');
-		if (typeof context.Backbone.View === 'undefined') throw new Error('Backbone environment not loaded');
-		if (typeof context.$ !== 'function') throw new Error('jQuery environment not loaded');
-		fn(context.$$, context.Backbone, context.$);
-	}
+    if (typeof context.define === 'function' && context.define.amd &&
+            typeof context._$$_backstrap_built_flag === 'undefined') {
+        context.define('backstrap/dispatcher', ['backstrap', 'backbone', 'jquery'], fn);
+    } else if (typeof context.module === 'object' && typeof context.module.exports === 'object') {
+        context.module.exports = fn(require('backstrap'), require('backbone'), require('jquery'));
+    } else {
+        if (typeof context.$$ !== 'function') throw new Error('Backstrap environment not loaded');
+        if (typeof context.$ !== 'function') throw new Error('jQuery environment not loaded');
+        fn(context.$$, context.$);
+    }
 }(this));
