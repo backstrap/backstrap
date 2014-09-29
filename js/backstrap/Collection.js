97036c050d3761299d1225841c1f840b1f4912cb
Kevin Perry <perry@princeton.edu>
2014-09-29 09:26:30 -0500

    refresh debugging.

--- js/backstrap/Collection.js
+++ js/backstrap/Collection.js
@@ -12,9 +12,10 @@
           autoRefresh: false
         },
 
-        initialize: function(options) {
-	    Backbone.Collection.prototype.initialize.call(this, options);
-            if(this.options.autoRefresh) {
+        initialize: function(model, options) {
+            // NOOP Backbone.Collection.prototype.initialize.call(this, model, options);
+            this.options = this.options ? _({}).extend(this.options, options) : options;
+            if (this.options.autoRefresh) {
               dispatcher.startRefresh(this);
             }  
         },
@@ -27,26 +28,26 @@
               dispatcher.startRefresh(this);
         },
 
-	refresh: function () {
-		this.fetch();
-	}
+        refresh: function () {
+            this.fetch();
+        }
 
     }));
   };
 
-	if (typeof context.define === "function" && context.define.amd &&
-			typeof context._$$_backstrap_built_flag === 'undefined') {
-		define("backstrap/Collection", ["backstrap", "backstrap/dispatcher", "backbone"], function ($$) {
-			return fn($$);
-		});
-	} else if (typeof context.module === "object" && typeof context.module.exports === "object") {
-		module.exports = fn(require("backstrap"), require('backstrap/dispatcher'), require('Backbone'));
-	} else {
-		if (typeof context.$$ !== 'function') throw new Error('Backstrap environment not loaded');
-		if (typeof context.$$.dispatcher !== 'object') throw new Error('Backstrap dispatcher not loaded');
-		if (typeof context.Backbone.View === 'undefined') throw new Error('Backbone environment not loaded');
-		fn(context.$$, context.$$.dispatcher, context.Backbone);
-	}
+    if (typeof context.define === "function" && context.define.amd &&
+            typeof context._$$_backstrap_built_flag === 'undefined') {
+        define("backstrap/Collection", ["backstrap", "backstrap/dispatcher", "backbone"], function ($$) {
+            return fn($$);
+        });
+    } else if (typeof context.module === "object" && typeof context.module.exports === "object") {
+        module.exports = fn(require("backstrap"), require('backstrap/dispatcher'), require('Backbone'));
+    } else {
+        if (typeof context.$$ !== 'function') throw new Error('Backstrap environment not loaded');
+        if (typeof context.$$.dispatcher !== 'object') throw new Error('Backstrap dispatcher not loaded');
+        if (typeof context.Backbone.View === 'undefined') throw new Error('Backbone environment not loaded');
+        fn(context.$$, context.$$.dispatcher, context.Backbone);
+    }
 }(this));
 
 
