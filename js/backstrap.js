c1b8b39b765e47fe1d58f298b4e49208f911ac9e
Kevin Perry <perry@princeton.edu>
2014-10-11 07:11:54 -0400

    updated list of HTML tags

--- js/backstrap.js
+++ js/backstrap.js
@@ -335,28 +335,34 @@
         };
 
         // html 4 tags
-        var deprecatedTags = ['acronym', 'applet', 'basefont', 'big', 'center', 'dir',
-                              'font', 'frame', 'frameset', 'noframes', 'strike', 'tt', 'u', 'xmp'];
+        var deprecatedTags = ['acronym', 'applet', 'basefont', 'big', 'center',
+                    'dir', 'font', 'frame', 'frameset', 'noframes',
+                    'strike', 'tt', 'xmp'];
 
         // html 5 tags
-        var tags = ['a', 'abbr', 'address', 'area', 'article', 'aside', 'audio', 'b',
-                    'base', 'bdo', 'blockquote', 'body', 'br', 'button', 'canvas', 'caption',
-                    'cite', 'code', 'col', 'colgroup', 'command', 'datalist', 'dd', 'del',
-                    'details', 'dfn', 'div', 'dl', 'dt', 'em', 'embed', 'fieldset',
-                    'figcaption', 'figure', 'footer', 'form', 'h1', 'h2', 'h3', 'h4', 'h5',
-                    'h6', 'head', 'header', 'hgroup', 'hr', 'html', 'i', 'iframe', 'img',
-                    'input', 'ins', 'keygen', 'kbd', 'label', 'legend', 'li', 'link', 'map',
-                    'mark', 'menu', 'meta', 'meter', 'nav', 'noscript', 'object', 'ol',
-                    'optgroup', 'option', 'output', 'p', 'param', 'pre', 'progress', 'q', 'rp',
-                    'rt', 'ruby', 's', 'samp', 'script', 'section', 'select', 'small',
-                    'source', 'span', 'strong', 'style', 'sub', 'summary', 'sup', 'table',
-                    'tbody', 'td', 'textarea', 'tfoot', 'th', 'thead', 'time', 'title', 'tr',
-                    'ul', 'var', 'video', 'wbr'].concat(deprecatedTags);
+        var tags = ['a', 'abbr', 'address', 'area', 'article', 'aside', 'audio',
+                    'b', 'base', 'bdi', 'bdo', 'blockquote', 'body', 'br', 'button',
+                    'canvas', 'caption', 'cite', 'code', 'col', 'colgroup',
+                    'datalist', 'dd', 'del', 'details',
+                    'dfn', 'dialog', 'div', 'dl', 'dt',
+                    'em', 'embed', 'fieldset', 'figcaption', 'figure', 'footer', 'form',
+                    'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
+                    'head', 'header', 'hgroup', 'hr', 'html',
+                    'i', 'iframe', 'img', 'input', 'ins', 'kbd', 'keygen',
+                    'label', 'legend', 'li', 'link',
+                    'main', 'map', 'mark', 'menu', 'menuItem', 'meta', 'meter',
+                    'nav', 'noscript', 'object', 'ol', 'optgroup', 'option', 'output',
+                    'p', 'param', 'pre', 'progress', 'q', 'rp', 'rt', 'ruby',
+                    's', 'samp', 'script', 'section', 'select', 'small', 'source',
+                    'span', 'strong', 'style', 'sub', 'summary', 'sup',
+                    'table', 'tbody', 'td', 'textarea', 'tfoot', 'th',
+                    'thead', 'time', 'title', 'tr', 'track',
+                    'u', 'ul', 'var', 'video', 'wbr'].concat(deprecatedTags);
 
         // Bootstrap component pseudo-tags
         var bootstrapComponents = ['alert', 'badge', 'breadcrumb', 'button',
-                    'buttonGroup', 'buttonToolbar', 'caret', 'container', 'form',
-                    'formGroup', 'input', 'inputGroup', 'inputGroupAddon',
+                    'buttonGroup', 'buttonToolbar', 'caret', 'container',
+                    'form', 'formGroup', 'input', 'inputGroup', 'inputGroupAddon',
                     'jumbotron', 'label', 'linkList', 'linkListItem', 'list',
                     'listItem', 'media', 'pageHeader', 'pagination', 'panel',
                     'spanLabel', 'table', 'thumbnail', 'well'];
