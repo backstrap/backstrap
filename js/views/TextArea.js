5b8acfd0bc2cb8069ce35de0d3b30f1447f92033
Kevin Perry <perry@princeton.edu>
2016-10-20 19:17:52 -0400

    Fix TextArea's paste event handling; regularize Text* code

--- js/views/TextArea.js
+++ js/views/TextArea.js
@@ -9,99 +9,110 @@
 define("backstrap/views/TextArea", ["../core", "jquery", "underscore"], function ($$, $, _)
 {
     return ($$.TextArea = $$.views.TextArea = $$.View.extend({
-        options : {
+        options: {
             // id to use on the actual textArea 
-            textAreaId : null,
+            textAreaId: null,
 
             // disables the text area
-            disabled : false,
+            disabled: false,
 
-            tabIndex : null,
+            tabIndex: null,
 
             // a callback to invoke when a key is pressed within the text field
-            onKeyPress : $.noop,
+            onKeyPress: $.noop,
 
-            // if given, the text field will limit it's character count
-            maxLength : null 
+            // if given, the text field will limit its character count
+            maxLength: null 
         },
 
         // public accessors
-        textArea : null,
+        textArea: null,
 
-        initialize : function(options) {
+        initialize: function (options) {
             $$.View.prototype.initialize.call(this, options);
+
             this.mixin([$$.mixins.HasModel, $$.mixins.HasFormLabel,
                 $$.mixins.HasError, $$.mixins.HasFocus]);
+
+            _(this).bindAll('_refreshValue', '_optKeyPress', '_updateModel');
             this.$el.addClass('text_area form-group');
 
-            if(this.options.name){
+            if (this.options.name){
                 this.$el.addClass(this.options.name);
             }
+
+            this.textArea = $$.textarea({maxLength: this.options.maxLength});
+
+            $(this.textArea).keyup(this._optKeyPress).input(this._updateModel);
+
+            this._observeModel(this._refreshValue);
         },
 
-        render : function() {
+        render: function () {
             var value = (this.textArea && this.textArea.value.length) > 0 ? 
                 this.textArea.value : this.resolveContent();
 
             this.$el.empty();
 
-            this.textArea = $$.textarea({
-                id : this.options.textAreaId,
+            $(this.textArea).attr({
+                id: this.options.textAreaId,
                 className: 'form-control form-control-default',
-                tabIndex : this.options.tabIndex, 
-                placeholder : this.options.placeholder,
-                maxLength : this.options.maxLength}, value);
+                tabIndex: this.options.tabIndex,
+                placeholder: this.options.placeholder,
+                value: value
+            });
 
-            this._observeModel(_(this._refreshValue).bind(this));
-
-            this._parent = $$.div({className : 'textarea_wrapper'}, this.textArea);
-
-            this.el.appendChild(this.getFormLabel());
-            this.el.appendChild(this._parent);
+            this._parent = $$.div({className: 'textarea_wrapper'}, this.textArea);
 
             // add focusin / focusout
             this.setupFocus(this.textArea, this._parent);
 
-            this.setEnabled(!this.options.disabled);
+            this.el.appendChild(this.getFormLabel());
+            this.el.appendChild(this._parent);
 
-            $(this.textArea).keyup(_(function(e) {
-                _.defer(_(this._updateModel).bind(this));
-                if(_(this.options.onKeyPress).exists() && _(this.options.onKeyPress).isFunction()) {
-                    this.options.onKeyPress(e, this);
-                }
-            }).bind(this));
+            this.setEnabled(!this.options.disabled);
 
             return this;
         },
 
-        getValue : function() {
+        getValue: function () {
             return this.textArea.value;
         },
 
-        setValue : function(value) {
+        setValue: function (value) {
             $(this.textArea).empty();
             this.textArea.value = value;
             this._updateModel();
         },
 
         // sets the enabled state
-        setEnabled : function(enabled) {
-            if(enabled) {
+        setEnabled: function (enabled) {
+            if (enabled) {
                 this.$el.removeClass('disabled');
             } else {
                 this.$el.addClass('disabled');
             }
+
             this.textArea.disabled = !enabled;
         },
 
-        _updateModel : function() {
+        _optKeyPress: function (e) {
+            var f = this.options.onKeyPress;
+
+            if (_(f).exists() && _(f).isFunction()) {
+                f(e, this);
+            }
+        },
+
+        _updateModel: function () {
             _(this.model).setProperty(this.options.content, this.textArea.value);
         },
 
-        _refreshValue : function() {
+        _refreshValue: function () {
             var newValue = this.resolveContent();
-            if(this.textArea && this.textArea.value !== newValue) {
-                this.textArea.value = _(newValue).exists() ? newValue : null;
+
+            if (this.textArea && this.textArea.value !== newValue) {
+                this.textArea.value = _(newValue).exists() ? newValue : '';
             }
         }
     }));
--- js/views/TextField.js
+++ js/views/TextField.js
@@ -9,111 +9,120 @@
 define("backstrap/views/TextField", ["../core", "jquery", "underscore"], function ($$, $, _)
 {
     return ($$.TextField = $$.views.TextField = $$.View.extend({
-        options : {
-            // disables the input text
-            disabled : false,
-            
+        options: {
             // The type of input (text, password, number, email, etc.)
-            type : 'text',
+            type: 'text',
 
             // the value to use for both the name and id attribute 
             // of the underlying input element
-            name : null,
+            name: null,
+
+            // disables the input text
+            disabled: false,
 
             // the tab index to set on the underlying input field
-            tabIndex : null,
+            tabIndex: null,
 
             // a callback to invoke when a key is pressed within the text field
-            onKeyPress : $.noop,
+            onKeyPress: $.noop,
 
-            // if given, the text field will limit it's character count
-            maxLength : null
+            // if given, the text field will limit its character count
+            maxLength: null
         },
 
         // public accessors
-        input : null,
+        input: null,
 
-        initialize : function(options) {
+        initialize: function (options) {
             $$.View.prototype.initialize.call(this, options);
-            this.mixin([$$.mixins.HasModel, $$.mixins.HasGlyph, 
-                $$.mixins.HasFormLabel, $$.mixins.HasError, $$.mixins.HasFocus]);
-            _(this).bindAll('_refreshValue');
-        
+
+            this.mixin([$$.mixins.HasModel, $$.mixins.HasFormLabel, $$.mixins.HasGlyph,
+                $$.mixins.HasError, $$.mixins.HasFocus]);
+
+            _(this).bindAll('_refreshValue', '_optKeyPress', '_updateModel');
             this.$el.addClass('text_field form-group');
-            if(this.options.name){
+
+            if (this.options.name){
                 this.$el.addClass(this.options.name);
             }
 
-            this.input = $$.input({maxLength : this.options.maxLength});
+            this.input = $$.input({maxLength: this.options.maxLength});
 
-            $(this.input).keyup(_(function(e) {
-                if(_(this.options.onKeyPress).exists() && _(this.options.onKeyPress).isFunction()) {
-                    this.options.onKeyPress(e, this);
-                }
-            }).bind(this)).input(_(this._updateModel).bind(this));
+            $(this.input).keyup(this._optKeyPress).input(this._updateModel);
 
             this._observeModel(this._refreshValue);
         },
 
-        render : function() {
+        render: function () {
             var value = (this.input && this.input.value.length) > 0 ? 
                 this.input.value : this.resolveContent();
 
             this.$el.empty();
 
             $(this.input).attr({
-                type : this.options.type ? this.options.type : 'text',
-                name : this.options.name,
-                id : this.options.name,
-                tabIndex : this.options.tabIndex,
-                placeholder : this.options.placeholder,
-                pattern : this.options.pattern,
-                value : value});
+                id: this.options.name,
+                type: this.options.type ? this.options.type : 'text',
+                name: this.options.name,
+                tabIndex: this.options.tabIndex,
+                placeholder: this.options.placeholder,
+                pattern: this.options.pattern,
+                value: value
+            });
 
             // insert glyph if exist
-            this._parent = $$.div({className : 'text_wrapper'});
+            this._parent = $$.div({className: 'text_wrapper'});
             var glyphLeftClassName = this.resolveGlyph(this.model, this.options.glyphLeftClassName);
             var glyphRightClassName = this.resolveGlyph(this.model, this.options.glyphRightClassName);
             this.insertGlyphLayout(glyphLeftClassName, glyphRightClassName, this.input, this._parent);
-            
+
             // add focusin / focusout
             this.setupFocus(this.input, this._parent);
-            
+
             this.el.appendChild(this.getFormLabel());
             this.el.appendChild(this._parent);
-            
+
             this.setEnabled(!this.options.disabled);
 
             return this;
         },
 
-        getValue : function() {
+        getValue: function () {
             return this.input.value;
         },
 
-        setValue : function(value) {
+        setValue: function (value) {
             this.input.value = value;
             this._updateModel();
         },
 
         // sets the enabled state
-        setEnabled : function(enabled) {
-            if(enabled) { 
+        setEnabled: function (enabled) {
+            if (enabled) {
                 this.$el.removeClass('disabled');
             } else {
                 this.$el.addClass('disabled');
             }
+
             this.input.disabled = !enabled;
         },
 
-        _updateModel : function() {
+        _optKeyPress: function (e) {
+            var f = this.options.onKeyPress;
+
+            if (_(f).exists() && _(f).isFunction()) {
+                f(e, this);
+            }
+        },
+
+        _updateModel: function () {
             _(this.model).setProperty(this.options.content, this.input.value);
         },
 
-        _refreshValue : function() {
+        _refreshValue: function () {
             var newValue = this.resolveContent();
+
             if(this.input && this.input.value !== newValue) {
-                this.input.value = _(newValue).exists() ? newValue : "";
+                this.input.value = _(newValue).exists() ? newValue : '';
             }
         }
     }));
