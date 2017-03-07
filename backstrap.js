65e057a5616b9f057e760c97531fab46fa4046d8
Kevin Perry <perry@princeton.edu>
2017-03-07 17:47:01 -0500

    AMD Bugfix in backstrap.js

--- backstrap.js
+++ backstrap.js
@@ -1,17 +1,16 @@
-import './backstrap/core.js';
-//import './backstrap/ajaxPrefilter.js';
-import './backstrap/input_event.js';
-import './backstrap/dispatcher.js';
-import './backstrap/Collection.js';
-//import './backstrap/DragSession.js';
-import './backstrap/LocalCache.js';
-import './backstrap/Model.js';
-//import './backstrap/PagedCollection.js';
-import './backstrap/View.js';
-import './backstrap/components.js';
-import './backstrap/mixins.js';
-import './backstrap/views.js';
 
-define('backstrap', ['./backstrap/core.js'], function ($$) {
+define([
+    './backstrap/core.js',
+    './backstrap/input_event.js',
+    './backstrap/dispatcher.js',
+    './backstrap/Collection.js',
+    './backstrap/LocalCache.js',
+    './backstrap/Model.js',
+    './backstrap/View.js',
+    './backstrap/components.js',
+    './backstrap/mixins.js',
+    './backstrap/views.js'
+], function ($$) {
     return $$;
 });
+
